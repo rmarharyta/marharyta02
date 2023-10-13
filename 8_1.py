f047e2b0e95c17974ee98c6bf5767823e9bec306
Marharyta Rublenko <145103782+rmarharyta@users.noreply.github.com>
2023-10-13 21:26:41 +0300

    Create 8_1.py

---
+++ 8_1.py
@@ -0,0 +1,19 @@
+#варіант 20
+N=int(input("Введіть кількість слів: "))
+
+word= []
+
+for i in range(N):
+    words= input("Введіть слово: ")
+    s= str.capitalize(words)
+    word.append(s)
+
+list=[]
+for s in word:
+    if s not in list:
+        list.append(s)
+
+
+list_str = " ".join(list)
+
+print(list_str)
