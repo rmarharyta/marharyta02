fbaaf659ce9b9198e0073c74465829dfdaa9683b
Marharyta Rublenko <145103782+rmarharyta@users.noreply.github.com>
2023-11-22 11:53:44 +0200

    Update 19_1.py

--- 19_1.py
+++ 19_1.py
@@ -6,10 +6,8 @@ with open("vector.csv", "r", encoding="cp1251") as f:
     df = pd.read_csv(f, sep=',', encoding="cp1251")
 
 df["Lenght"] = np.sqrt(df['CoordinateX']**2 + df['CoordinateY']**2)
-df["Lenght"] = df["Lenght"].apply(
-    lambda x: round(x, 2) if x % 1 != 0 else int(x))
-array_data = np.column_stack((df["Vector"].to_numpy(), df["CoordinateX"].to_numpy(
-), df["CoordinateY"].to_numpy(), df["Lenght"].to_numpy()))
+df["Lenght"] = df["Lenght"].apply(lambda x: round(x, 2) if x % 1 != 0 else int(x))
+array_data = np.column_stack((df["Vector"].to_numpy(), df["CoordinateX"].to_numpy(), df["CoordinateY"].to_numpy(), df["Lenght"].to_numpy()))
 
 columns = ["Vector", "CoordinateX", "CoordinateY", "Length"]
 df1 = pd.DataFrame(array_data, columns=columns)
